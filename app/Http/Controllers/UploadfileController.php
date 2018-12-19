32f597a25ab9d066326c057b9a4d357c0db3efbb
Yun-Jan <yt266@njit.edu>
2018-12-19 14:52:55 -0500

    Created uploadfile controller

---
+++ app/Http/Controllers/UploadfileController.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\File;
+use Illuminate\Foundation\Bus\DispatchesJobs;
+use Illuminate\Http\UploadedFile;
+use Illuminate\Routing\Controller as BaseController;
+use Illuminate\Foundation\Validation\ValidatesRequests;
+use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Storage;
+
+class UploadfileController extends Controller
+{
+    public function upload(Request $request)
+    {
+
+        if ($request->isMethod('POST')){
+            //var_dump($_FILES);
+            $file=$request->file('file');
+
+            //If file uploaded successfully
+            if ($file->isValid()){
+                //original file name
+                $originalNmae=$file->getClientOriginalName();
+                //extension file name
+                $extension=$file->getClientOriginalExtension();
+                //MineType
+                $type=$file->getClientMimeType();
+                //Temporary path
+                $realPath=$file->getRealPath();
+
+                $filename=date('Y-m-d-H-i-s') . '-' . uniqid() . '.' . $extension;
+                $bool=Storage::disk('uploads')->put($filename, file_get_contents($realPath));
+
+                var_dump($bool);
+                echo ('File Uploaded Successfully!');
+            }
+            exit;
+        }
+        return view('uploadfile');
+
+    }
+
+
+}
